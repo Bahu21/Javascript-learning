f820964f01ffa988e76092bff1ee85e055336af1
Bahu21 <155416237+Bahu21@users.noreply.github.com>
2026-09-16 09:54:39 +0000

    datatype conversions

---
+++ .devcontainer/01-basics/03_datatypeConversions.js
@@ -0,0 +1,33 @@
+let age = "2312"
+console.log(typeof age);
+
+let numberAge = Number(age)
+console.log(typeof numberAge);
+console.log(numberAge);
+
+// if we convert 32sd to number then NaN will be returned that is not a number
+
+let isloggedin = "dfdsf"
+
+console.log(typeof isloggedin);
+ 
+let isboolean = Boolean(isloggedin)
+console.log(isboolean)
+
+/*
+true => true
+false => false
+1 => true
+0 => false
+"bahulashwa" => true
+"" => false
+*/
+
+let pin = 1234
+console.log(pin)
+console.log(typeof pin)
+
+let newpin= String(pin)
+console.log(pin)
+console.log(typeof newpin)
+
